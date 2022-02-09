d47a7d7090b5419df668529151c67dba0523532b
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-09 15:23:23 +0100

    push

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const app = new Vue (
-    {
+const app = new Vue({
         el: "#root",
         data: {
             messaggio: 'Boolean',
-            image: 'img/boolean_logo.png'
+            image: 'img/boolean_logo.png',
+            classe: "text-italic"
+        },
+        // creo una funzione che al click cambi font della parola
+        methods: {
+            changeFont: function () {
+
+                if (this.classe == 'text-normal') {
+                    this.classe = 'text-italic';
+                } else {
+                    this.classe = 'text-normal';
+                }
+            }
         }
     }
 
